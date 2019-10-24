87b97bc9de025e6b0b16aa9c7b0b2c0d2032d3cb
Kevin Li <46378390+adularescence@users.noreply.github.com>
2019-10-23 20:41:07 -0400

    feat: ability to select wait times for n number of guests

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 // get all wait times
 app.get('/api/v1/wait_times', (req, res) => {
   res.status(200).send({
@@ -18,6 +19,32 @@ app.get('/api/v1/wait_times', (req, res) => {
   });
 });
 
+app.get('/api/v1/wait_times/:guests', (req, res) => {
+  const guests = parseInt(req.params.guests);
+  const results = [];
+  db.map(wait_time => {
+    if (wait_time.guests === guests) {
+      results.push(wait_time);
+    } else {
+      console.log(wait_time)
+    }
+  });
+  if (results.length !== 0) {
+    return res.status(200).send({
+      success: 'true',
+      message: `wait times for a guest count of ${guests} retrieved successfully`,
+      results
+    });
+  } else {
+    return res.status(404).send({
+      success: 'false',
+      message: `there are no wait times for a guest count of ${guests}`
+    });
+  }
+});
+
+
+// post to wait times
 app.post('/api/v1/wait_times', (req, res) => {
   if (!req.body.guests) {
     return res.status(400).send({
@@ -37,7 +64,7 @@ app.post('/api/v1/wait_times', (req, res) => {
   }
 
   const wait_time = {
-    guests: req.body.guests,
+    guests: parseInt(req.body.guests),
     timestamp: req.body.timestamp,
     table: req.body.table
   };
