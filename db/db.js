edad54eb0d53c93fe9ed2faca9d203928f9f93bd
Kevin Li <46378390+adularescence@users.noreply.github.com>
2019-11-04 18:44:59 -0500

    feat: add all tables in restaurant

--- db/db.js
+++ db/db.js
@@ -10,9 +10,162 @@ const guestList = [
 
 ];
 
-// this is going to be a pita
 const tableList = [
-
+  {
+    "number": "1",
+    "type": "table",
+    "count": "4"
+  },
+  {
+    "number": "2",
+    "type": "table",
+    "count": "4"
+  },
+  {
+    "number": "3",
+    "type": "table",
+    "count": "4"
+  },
+  {
+    "number": "4",
+    "type": "table",
+    "count": "2"
+  },
+  {
+    "number": "5",
+    "type": "table",
+    "count": "4"
+  },
+  {
+    "number": "22",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "24",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "26",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "31",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "32",
+    "type": "booth",
+    "count": "6"
+  },
+  {
+    "number": "33",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "34",
+    "type": "booth",
+    "count": "6"
+  },
+  {
+    "number": "35",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "36",
+    "type": "booth",
+    "count": "6"
+  },
+  {
+    "number": "41",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "42",
+    "type": "booth",
+    "count": "6"
+  },
+  {
+    "number": "43",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "44",
+    "type": "booth",
+    "count": "6"
+  },
+  {
+    "number": "45",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "46",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "51",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "52",
+    "type": "booth",
+    "count": "4"
+  },
+  {
+    "number": "53",
+    "type": "deuce",
+    "count": "2"
+  },
+  {
+    "number": "54",
+    "type": "deuce",
+    "count": "2"
+  },
+  {
+    "number": "55",
+    "type": "deuce",
+    "count": "2"
+  },
+  {
+    "number": "61",
+    "type": "hightop",
+    "count": "2"
+  },
+  {
+    "number": "62",
+    "type": "hightop",
+    "count": "2"
+  },
+  {
+    "number": "63",
+    "type": "hightop",
+    "count": "2"
+  },
+  {
+    "number": "64",
+    "type": "hightop",
+    "count": "2"
+  },
+  {
+    "number": "65",
+    "type": "deuce",
+    "count": "2"
+  },
+  {
+    "number": "66",
+    "type": "deuce",
+    "count": "2"
+  }
 ];
 
 module.exports = {
